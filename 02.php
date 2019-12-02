9d634fc182f6eacd9dac328476c0fab3fb241b37
jdlcgarcia <jcruzgar@gmail.com>
2019-12-02 17:55:28 +0100

    second solution

--- 02.php
+++ 02.php
@@ -2,7 +2,19 @@
 include_once 'class/Intcode.php';
 $file_lines = file('input/02.txt');
 
-$intcode = new Intcode(explode(",",$file_lines[0]));
+$input = explode(",", $file_lines[0]);
+$intcode = new Intcode($input);
 $intcode->setInput(12,2);
 $intcode->operate();
-echo $intcode->getOutput()."\n";
+echo "Solution for nº1: ".$intcode->getOutput()."\n";
+for ($noun = 0; $noun <= 99; $noun++) {
+    for ($verb = 0; $verb <= 99; $verb++) {
+        $intcode = new Intcode($input);
+        $intcode->setInput($noun,$verb);
+        $intcode->operate();
+        if ($intcode->getOutput() == 19690720) {
+            echo "Solution for nº2: \n - Noun: ".$noun."\n - Verb: ".$verb."\n";
+            echo "100 * noun + verb = ".(100*$noun+$verb)."\n";
+        }
+    }
+}
