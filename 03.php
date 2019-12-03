bdbf2a7797f8d97dcef56742df9ca07ee66831ad
Juan de la Cruz García García <jc.garcia@kauffeld-lorenzo.de>
2019-12-03 16:03:17 +0100

    wire class with path

--- 03.php
+++ 03.php
@@ -1,7 +1,15 @@
 <?php
 include_once 'class/WirePanel.php';
-$file_lines = file('input/demo.txt');
-//$input = explode(",", $file_lines[0]);
-//$intcode = new Intcode($input);
+include_once 'class/Wire.php';
 $wirePanel = new WirePanel();
 $wirePanel->drawStatus();
+
+$file_lines = file('input/demo.txt');
+foreach($file_lines as $wireInput) {
+    $wire = new Wire(explode(",", $wireInput));
+    var_dump($wire);
+    //$wirePanel->addWire($wire);
+    //$wirePanel->drawStatus();
+}
+//$input = explode(",", $file_lines[0]);
+//$intcode = new Intcode($input);
---
+++ class/Wire.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Wire
+{
+    private $path;
+    /**
+     * Wire constructor.
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+}
