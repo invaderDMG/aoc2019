8f54cab8f1acfc4def04f5c9a0e7479ba6782905
jdlcgarcia <jcruzgar@gmail.com>
2019-12-04 13:37:00 +0100

    Second answer

--- 04.php
+++ 04.php
@@ -3,3 +3,4 @@ include_once 'class/PasswordChecker.php';
 $input = "234208-765869";
 $passwordChecker = new PasswordChecker();
 echo "Number of possible passwords: ".$passwordChecker->getPossiblePasswords($input)."\n";
+echo "Number of possible passwords: ".$passwordChecker->getPossiblePasswordsWithExtraRules($input)."\n";
--- class/PasswordChecker.php
+++ class/PasswordChecker.php
@@ -64,4 +64,40 @@ class PasswordChecker
         }
         return true;
     }
+
+    public function getPossiblePasswordsWithExtraRules(string $input)
+    {
+        list($begin, $end) = explode("-", $input);
+        $goodCandidates = 0;
+        for($i = $begin; $i <= $end; $i++) {
+            if ($this->checkCandidate($i) && $this->extraRules($i)) {
+                $goodCandidates++;
+            }
+        }
+        return $goodCandidates;
+    }
+
+    private function extraRules($candidate)
+    {
+        $digits = str_split($candidate, 1);
+        return $this->fifthCondition($digits);
+    }
+
+    private function fifthCondition(array $digits)
+    {
+        $summary = [];
+        foreach($digits as $digit) {
+            if (!isset($summary[$digit])) {
+                $summary[$digit] = 0;
+            }
+            $summary[$digit]++;
+        }
+        //remove repetitions > 2 and no repetitions at all
+        foreach($summary as $digit => $repetitions) {
+            if ($repetitions > 2 || $repetitions == 1) {
+                unset($summary[$digit]);
+            }
+        }
+        return sizeof($summary) > 0;
+    }
 }
